e013050b18b636396bc2b2f0edd6e900890676f2
Xulxuvar <teridax1306@gmail.com>
2020-11-10 23:08:43 -0600

    Added Generic Class for world objects

---
+++ Scripts/LevelEntity.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	MultiplayerManager.in_world.append(self)
+	pass # Replace with function body.
+
+func _dispose():
+	MultiplayerManager.in_world.remove(MultiplayerManager.in_world.bsearch(self));
+	queue_free();
+
+func _exit_tree():
+	_dispose();
+
+func bounce_back(step :float):
+	set_position(Vector2(get_position().x+step,get_position().y))
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
