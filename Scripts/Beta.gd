31ee489bea71f21bd23f93eebd70cd391dd442eb
rchin157 <rchin@ualberta.ca>
2020-11-11 02:00:50 -0700

    punpun

--- Scripts/Beta.gd
+++ Scripts/Beta.gd
@@ -8,29 +8,96 @@ extends "res://Scripts/LevelEntity.gd"
 var running = false
 var attacker
 var direction
+var passiveDirection
 var numAttackers = 0
 var speed = 100
+var animator
+var facingRight = true
+var walkTimer = 2
+var walking = false
+
+var influences = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	animator = get_node("AnimatedSprite")
+	passiveDirection = Vector2.LEFT
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(running)
+	#print(running)
 	if running:
-		direction = (get_position() - attacker.get_position()).normalized()
-		move_and_slide(direction * 100)
+		walkTimer = 8
+		walking = false
+		direction = Vector2.ZERO
+		for body in influences:
+			direction += get_position() - body.get_position()
+		direction = direction.normalized() * speed
+		if facingRight and direction.x < 0:
+			animator.set_animation("chaseLeft")
+			facingRight = false
+		elif not facingRight and direction.x >= 0:
+			animator.set_animation("chaseRight")
+			facingRight = true
+		move_and_slide(direction)
+	walkTimer -= delta
+	if walkTimer <= 0:
+		togglewalk()
+	if walking:
+		move_and_slide(passiveDirection * (speed / 3))
 
 
 func _on_Area2D_body_entered(body):
 	running = true
+	influences.append(body)
+	if body.get_position().x > get_position().x:
+		facingRight = false
+		animator.set_animation("chaseLeft")
+	else:
+		animator.set_animation("chaseRight")
+		facingRight = true
 	attacker = body
 	numAttackers += 1
+	
 
 
 func _on_Area2D_body_exited(body):
+	influences.erase(body)
 	numAttackers -= 1
 	if numAttackers == 0:
 		running = false
+		if facingRight:
+			animator.set_animation("idleRight")
+		else:
+			animator.set_animation("idleLeft")
+
+func togglewalk():
+	if walking:
+		if facingRight:
+			animator.set_animation("idleRight")
+		else:
+			animator.set_animation("idleLeft")
+		walking = false
+		walkTimer = 5
+		if passiveDirection == Vector2.RIGHT:
+			passiveDirection = Vector2.DOWN;
+		elif passiveDirection == Vector2.DOWN:
+			passiveDirection = Vector2.LEFT
+		elif passiveDirection == Vector2.LEFT:
+			passiveDirection = Vector2.UP
+		else:
+			passiveDirection = Vector2.RIGHT
+	else:
+		if passiveDirection == Vector2.RIGHT:
+			facingRight = true
+			animator.set_animation("walkRight")
+		elif passiveDirection == Vector2.LEFT:
+			facingRight = false
+			animator.set_animation("walkLeft")
+		else:
+			animator.set_animation("walkRight")
+		walking = true
+		walkTimer = 2
+		
