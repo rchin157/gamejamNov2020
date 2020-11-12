36958ca428dc520912844a1322e28711cab893ae
rchin157 <rchin@ualberta.ca>
2020-11-12 04:14:52 -0700

    screens more

---
+++ Scripts/Main Menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene("res://Level/OpeningMenu.tscn")
+	pass # Replace with function body.
+
+
+func _on_info_pressed():
+	get_tree().change_scene("res://Level/Info.tscn")
+	pass # Replace with function body.
