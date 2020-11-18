fe419005daa31170a37dcdb3b7cf0d956a61e61e
rchin157 <rchin@ualberta.ca>
2020-11-18 15:45:41 -0700

    embrace our google overlords

---
+++ Scripts/Signup.gd
@@ -0,0 +1,26 @@
+extends Control
+
+onready var http : HTTPRequest = $HTTPRequest
+onready var email : LineEdit = $VBoxContainer/HBoxContainer/LineEdit
+onready var password : LineEdit = $VBoxContainer/HBoxContainer2/LineEdit
+onready var notification : Label = $notification
+
+func _on_signup_pressed():
+	if email.text.empty() or password.text.empty():
+		notification.text = "Invalid password or username"
+		return
+	Firebase.register(email.text, password.text, http)
+
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var response_body = JSON.parse(body.get_string_from_ascii())
+	if response_code != 200:
+		notification.text = response_body.result.error.message.capitalize()
+	else:
+		notification.text = "Signed Up"
+		yield(get_tree().create_timer(2.0), "timeout")
+		get_tree().change_scene("res://Level/Firebase/Login.tscn")
+
+
+func _on_Back_pressed():
+	get_tree().change_scene("res://Level/Firebase/AuthStart.tscn")
